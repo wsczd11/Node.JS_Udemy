ed2082a9ecfc1d43382580bebdaeb4973442b7b3
Zhedong CAO <83537039+wsczd11@users.noreply.github.com>
2021-12-21 15:03:44 +1300

    add formating tool Prettier into VS and project.

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,37 +1,35 @@
-const fs = require('fs')
+const fs = require('fs');
 const http = require('http');
 const url = require('url');
 const slugfify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 const { default: slugify } = require('slugify');
 
-
 //////////////////////////////////
-// File 
+// File
 
 // // Blocking, synchronous way
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
-// console.log(textIn)
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
 // const textOut = `This is waht we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File written!');
 
 // // Non-blocking, asynchronous way
 // fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-//     if (err) return console.log("ERROR! 🤯")
-//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//         console.log(data2);
-//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-//             console.log(data3);
+//   if (err) return console.log('ERROR! 🤯');
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-//                 console.log('Your file has been written 😄');
-//             });
-//         });
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('Your file has been written 😄');
+//       });
 //     });
+//   });
 // });
 // console.log('Will resd file!');
-
 
 //////////////////////////////////
 // Server
@@ -41,47 +39,47 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'u
 const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObj = JSON.parse(data)
+const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 console.log(slugs);
 
 const server = http.createServer((req, res) => {
-    const { query, pathname } = url.parse(req.url, true);
+  const { query, pathname } = url.parse(req.url, true);
 
-    //Overview page
-    if (pathname === '/overview' || pathname === '/') {
-        res.writeHead(200, { 'Content-type': 'text/html' });
+  //Overview page
+  if (pathname === '/overview' || pathname === '/') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-        const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHtml);
+    const cardHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHtml);
 
-        res.end(output);
+    res.end(output);
 
-        //Pruduct page
-    } else if (pathname === '/product') {
-        res.writeHead(200, { 'Content-type': 'text/html' });
+    //Pruduct page
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-        const product = dataObj[query.id];
-        const output = replaceTemplate(tempProduct, product);
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
 
-        res.end(output);
+    res.end(output);
 
-        //API
-    } else if (pathname == '/api') {
-        res.writeHead(200, { 'Content-type': 'application/json' });
-        res.end(data);
+    //API
+  } else if (pathname == '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
 
-        //Not found
-    } else {
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'hello-world'
-        });
-        res.end('<h1>Page not found!</h1>');
-    }
+    //Not found
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000!');
+  console.log('Listening to requests on port 8000!');
 });
